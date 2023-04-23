84b800b4328fc8b926e153ab9767221802880e2b
tanaka0079 <34793405+nishizumi-lab@users.noreply.github.com>
2023-04-24 01:04:52 +0900

    Update main.js

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -15,7 +15,7 @@ $(window).resize(sectionHeight);
 $(function() {
   $("section h1, section h2, section h3").each(function(){
     $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]/g, function(match){return match.charCodeAt(0);}));
+    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-'));
     $("nav ul li:first-child a").parent().addClass("active");
   });
 
